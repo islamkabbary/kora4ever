db747a3f3219849262329775641297372bdf0135
islamkabbary <islamkabbary@gmail.com>
2023-11-08 23:17:39 +0200

    fix

--- resources/views/Dashboard/test.blade.php
+++ resources/views/Dashboard/test.blade.php
@@ -1,31 +1,6 @@
-<div class='posts-body posts-content' id='postsacs' itemprop='description articleBody'>
-    <div id='postAD-top'></div>
-    <div id='aim17921603386324665535'></div>
-    <br />
-    <div id='aim27921603386324665535'>
-        <div class="separator" style="clear: both;"><a
-                href="https://blogger.googleusercontent.com/img/a/AVvXsEhdNypuwLzZKcryhpSpVe3Ffoo7pJMxFPb28-G2J5-w2yieTgDlBPUF0J2s19C7Smw-a9QnR9TQA5GLD4S_kqo_s_85Cp8SJ60B9rR32rr_9KehR5qJHwA_pzk0AFUmhhe6bEWsAD9hKIJYrk05oZA74UttRBBGlVy1LtCuih3tLdlkyrl2t0Xw8TMStg"
-                style="display: block; padding: 1em 0px; text-align: center;"><img border="0"
-                    data-original-height="720" data-original-width="1078" height="214"
-                    src="https://blogger.googleusercontent.com/img/a/AVvXsEhdNypuwLzZKcryhpSpVe3Ffoo7pJMxFPb28-G2J5-w2yieTgDlBPUF0J2s19C7Smw-a9QnR9TQA5GLD4S_kqo_s_85Cp8SJ60B9rR32rr_9KehR5qJHwA_pzk0AFUmhhe6bEWsAD9hKIJYrk05oZA74UttRBBGlVy1LtCuih3tLdlkyrl2t0Xw8TMStg=w320-h214-rw"
-                    width="320" /></a></div>
-        <div style="text-align: right;">
-            <!-- adsense -->
-            <iframe allowfullscreen="true" scrolling="no" src="https://m.dimakora.com/p/playr-4.html"
-                style="width:100%; height:500px;"></iframe>
-        </div>
+<div>
+    <div style="text-align: right;">
+        <iframe allowfullscreen="true" scrolling="no" src="https://m.dimakora.com/p/playr-4.html"
+            style="width:100%; height:500px;"></iframe>
     </div>
-    <script type='text/javascript'>
-        var obj0 = document.getElementById("aim17921603386324665535");
-        var obj1 = document.getElementById("aim27921603386324665535");
-        var s = obj1.innerHTML;
-        var r = s.search(/\x3C!-- adsense --\x3E/igm);
-        if (r > 0) {
-            obj0.innerHTML = s.substr(0, r);
-            obj1.innerHTML = s.substr(r + 16);
-        }
-    </script>
-    <font size='0'><a href=' '></a></font>
-    <div id='postAD-bottom'></div>
-    <div style='clear: both;'></div>
 </div>
