2f6991d5dcb8f940ee9b1ebaad47c47373ffe537
islamkabbary <islamkabbary@gmail.com>
2023-11-09 10:06:04 +0200

    fix

--- resources/views/Dashboard/test.blade.php
+++ resources/views/Dashboard/test.blade.php
@@ -1,6 +1,6 @@
-<div>
+{{-- <div>
     <div style="text-align: right;">
         <iframe allowfullscreen="true" scrolling="no" src="https://m.dimakora.com/p/playr-4.html"
             style="width:100%; height:500px;"></iframe>
     </div>
-</div>
+</div> --}}
