bb65f9a98af28298fa20c9925ea4854bb456afec
islamkabbary <islamkabbary@gmail.com>
2023-11-24 22:17:14 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NewsController;
+use App\Http\Livewire\CreateNewsComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login', [AuthController::class,'login']);
+Route::post('/create-news', [CreateNewsComponent::class, 'add']);
+Route::post('logout', [AuthController::class,'logout']);
+Route::post('change-password', [AuthController::class,'change_password']);
