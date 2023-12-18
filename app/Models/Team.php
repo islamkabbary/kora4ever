23b2e164c218a05d66dada5c0e7b5ae4c7d0c70d
islamkabbary <islamkabbary@gmail.com>
2023-12-18 15:17:18 +0200

    fix

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -11,6 +11,6 @@ class Team extends Model
 
     public function leauges()
     {
-        return $this->belongsToMany(Leauge::class);
+        return $this->belongsToMany(Leauge::class, 'team_has_leauges');
     }
 }
