edf54bc34be1ef0d1fccba7eca5fd4c36b2ee8a3
islamkabbary <islamkabbary@gmail.com>
2024-01-06 10:44:56 +0200

    fix

--- app/Http/Livewire/BlockNewsComponent.php
+++ app/Http/Livewire/BlockNewsComponent.php
@@ -18,6 +18,7 @@ class BlockNewsComponent extends Component
             $q->where("show_from_the_five_main_news", false);
             $q->where("show_in_most_read", false);
         })->orderBy('created_at', 'desc')->take(4)->get();
+        dd($fourNews);
         return view('livewire.block-news-component', ['fourNews' => $fourNews, 'leauge' => $leauge]);
     }
 }
